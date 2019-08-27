314d0124f77a3efc90794fc80eb9a8537b4ca7f2
MELFI PEREZ <perezmelfi@outlook.com>
2019-08-27 13:33:33 -0700

    can successfully create a new user along with loggin in and out

---
+++ src/main/java/com/perezm27/CodeFellowship/config/UserDetailsServiceImpl.java
@@ -0,0 +1,20 @@
+package com.perezm27.CodeFellowship.config;
+
+import com.perezm27.CodeFellowship.models.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    ApplicationUserRepository applicationUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        return applicationUserRepository.findByUsername(username);
+    }
+}
