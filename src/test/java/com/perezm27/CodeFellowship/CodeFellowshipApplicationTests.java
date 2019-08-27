819f44cd7d0795e51ce86158fa490287f0435a05
MELFI PEREZ <perezmelfi@outlook.com>
2019-08-26 23:56:19 -0700

    scaffold

---
+++ src/test/java/com/perezm27/CodeFellowship/CodeFellowshipApplicationTests.java
@@ -0,0 +1,16 @@
+package com.perezm27.CodeFellowship;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodeFellowshipApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
